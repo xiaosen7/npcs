faf605da85bed4b6582d9e09a53cac9c7b83451d
wangshouren <1115808717@qq.com>
2024-07-07 17:15:53 +0800

    chore: pnpm nx

--- apps/large-file-upload/next.config.mjs
+++ apps/large-file-upload/next.config.mjs
@@ -1,15 +1,13 @@
-import isDocker from "is-docker";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASE_PATH,
   eslint: {
     ignoreDuringBuilds: true,
   },
   /**
    * standalone: after next build, then teh node_modules/next directory will be empty, I don't know why
    */
-  output: isDocker() ? "standalone" : undefined,
+  output: "standalone",
+  distDir: "dist",
 };
 
 export default nextConfig;
