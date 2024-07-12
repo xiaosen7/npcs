e5a235453c32368b52c77a91228b47d18ee65f62
wangshouren <1115808717@qq.com>
2024-07-12 20:48:02 +0800

    update deploy

--- apps/stackoverflow/deploy/setup-database.sh
+++ apps/stackoverflow/deploy/setup-database.sh
@@ -4,4 +4,4 @@ script_path=$(readlink -f "$0")
 script_dir=$(dirname "$script_path")
 
 docker network create “$script_dir/stackoverflow-network” > /dev/null 2>&1
-docker compose -f "$script_dir/docker-compose.db.yml" up
+docker compose -f "$script_dir/docker-compose.db.yml" up -d
