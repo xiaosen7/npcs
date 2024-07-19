33e9846028786c209b8ac94fc02299bca8f2ac46
wangshouren <1115808717@qq.com>
2024-07-19 09:25:24 +0800

    chore: add prettier/eslint lint staged

--- apps/notes/src/app/layout.tsx
+++ apps/notes/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { DefaultLayout } from "@/layout/default";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <DefaultLayout>{children}</DefaultLayout>
+      </body>
     </html>
   );
 }
--- apps/notes/tests/example.ts
+++
@@ -1,3 +0,0 @@
-test("base", () => {
-  expect({}).toEqual({});
-});
