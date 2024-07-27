402e6e8f3198fa883231ac02e20a54df9a90cd89
wangshouren <1115808717@qq.com>
2024-07-27 12:58:39 +0800

    update env keys

--- libs/shared/src/env/server.ts
+++ libs/shared/src/env/server.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().min(1),
-    CLERK_SECRET_KEY: z.string().min(1),
+    DATABASE_URL: z.string().min(1).optional(),
+    CLERK_SECRET_KEY: z.string().min(1).optional(),
   },
   experimental__runtimeEnv: process.env,
 });
