1ffad45db3c01c528a12d4ccd7b67f8285b02a2a
wangshouren <1115808717@qq.com>
2024-07-29 14:32:06 +0800

    fix(large-file-upload): fix styles

--- apps/large-file-upload/tailwind.config.ts
+++ apps/large-file-upload/tailwind.config.ts
@@ -2,7 +2,7 @@ import sharedConfig from "@npcs/tailwind-config";
 import type { Config } from "tailwindcss";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./app/**/*.{tsx,css}"],
+  content: ["./src/**/*.{tsx,css}"],
   presets: [sharedConfig],
 };
 
