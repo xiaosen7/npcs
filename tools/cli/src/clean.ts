a6d27d23be0b1464f7367463320089a7164e50bc
wangshouren <1115808717@qq.com>
2024-07-29 17:02:24 +0800

    feat(cli): print targets

--- tools/cli/src/clean.ts
+++ tools/cli/src/clean.ts
@@ -2,8 +2,20 @@ import { rimraf } from "rimraf";
 import { z } from "zod";
 import { Command, IOptionsValidation } from "./command";
 
+const targets = [
+  "node_modules",
+  "esm",
+  ".turbo",
+  ".rollup.cache",
+  "*.tsbuildinfo",
+  ".next",
+  "playwright-report",
+  "test-results",
+  "vitest.config.*.timestamp*",
+  "coverage",
+];
 const name = "clean";
-const description = "Clean files in current project.";
+const description = `Clean files in current project. targets: ${targets.join(", ")}`;
 const options = z.object({}) satisfies IOptionsValidation;
 
 export class CommandClean extends Command<typeof options> {
@@ -12,22 +24,8 @@ export class CommandClean extends Command<typeof options> {
   }
 
   async action() {
-    await rimraf(
-      [
-        "node_modules",
-        "esm",
-        ".turbo",
-        ".rollup.cache",
-        "*.tsbuildinfo",
-        ".next",
-        "playwright-report",
-        "test-results",
-        "vitest.config.*.timestamp*",
-        "coverage",
-      ],
-      {
-        glob: true,
-      },
-    );
+    await rimraf(targets, {
+      glob: true,
+    });
   }
 }
